cebbcd590c83848ec63ceac4bfcf81aa0cdfea89
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-08-24 10:48:43 -0300

    api composition pattern - rest template

---
+++ src/main/java/com/ead/course/services/impl/UtilsServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.services.UtilsService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UtilsServiceImpl implements UtilsService {
+
+    private static final String REQUEST_URI = "http://localhost:8087";
+
+    public String createUrl(UUID userId, Pageable pageable) {
+        return REQUEST_URI + "/users?courseId=" + userId + "&page=" + pageable.getPageNumber() + "&size="
+                + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(": ", ",");
+    }
+}
